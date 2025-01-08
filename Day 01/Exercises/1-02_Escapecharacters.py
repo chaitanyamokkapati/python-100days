ca8c67ec0c4edfcb823a693bfa46938559ff51e2
chaitanya_mokkapati <chaitanyamokkapati6@gmail.com>
2025-01-08 19:49:25 +0530

    Added 1-02_Escapecharacters.py

---
+++ Day 01/Exercises/1-02_Escapecharacters.py
@@ -0,0 +1,11 @@
+# Output :
+'''
+Hello World !!!
+Hello World !!!
+Hello World !!!
+'''
+print("Hello World !!!\nHello World !!!\nHello World !!!")
+print(" ")
+print("---" * 5)
+print(" ")
+print("Hello World !!!\n" * 3)
