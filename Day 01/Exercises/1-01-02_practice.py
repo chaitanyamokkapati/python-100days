d46a21f609d4f458c7d1dfb6270cab9640d50b3b
chaitanya_mokkapati <chaitanyamokkapati6@gmail.com>
2025-01-08 19:38:08 +0530

    Added 1-01-02_practice.py

---
+++ Day 01/Exercises/1-01-02_practice.py
@@ -0,0 +1,10 @@
+# Output as Folows
+'''
+Day 1 - Python Print Function
+The function is declared like this:
+print('What to print')
+
+'''
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('What to print')")
