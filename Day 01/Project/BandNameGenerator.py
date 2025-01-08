d8d718626de33c005202a3d1776466a9b8656e9c
chaitanya_mokkapati <chaitanyamokkapati6@gmail.com>
2025-01-09 04:26:43 +0530

    Added BandNameGenerator.py

---
+++ Day 01/Project/BandNameGenerator.py
@@ -0,0 +1,6 @@
+print("Welcome to Band Name Generator")
+
+userName = input("What is your Name: ")
+city = input("Which City do you live in? ")
+bandName = userName + " "+ city
+print("Your Band Name is = " + bandName)
