58deb147d0bfa99fcac468d8e9171e4cee3ae056
chaitanya_mokkapati <chaitanyamokkapati6@gmail.com>
2025-01-09 04:34:40 +0530

    Added 1-04_Variables.py

---
+++ Day 01/Exercises/1-04_Variables.py
@@ -0,0 +1,16 @@
+# Write a program that switches the values stored in the variables a and b.
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+print("a : " + a)
+print("b : " + b)
+
+''' 
+Wrong
+a = print("a = " + a)
+b = print("b = " + b)
+'''
