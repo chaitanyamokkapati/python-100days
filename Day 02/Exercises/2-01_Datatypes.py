606c1d5e5dedbff1d3eb3ebb009471e83f751892
chaitanya_mokkapati <chaitanyamokkapati6@gmail.com>
2025-01-09 04:55:45 +0530

    Added 2-01_Datatypes.py

---
+++ Day 02/Exercises/2-01_Datatypes.py
@@ -0,0 +1,13 @@
+# Data Types
+'''
+1. Interger
+2. Float
+3. String
+4. Boolean
+'''
+# String
+print("1234" + "4567") # Dont add but soes string concatenation
+# Subscript
+print("---- SUBSCRIPITING ----")
+print("Hello"[0])
+print("Hello"[-1]) # For last character
