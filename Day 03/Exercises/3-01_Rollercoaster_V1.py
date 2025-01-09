9e409aa57d7873f74ff03c7ec6ae934bae03e162
chaitanya_mokkapati <chaitanyamokkapati6@gmail.com>
2025-01-09 12:57:17 +0530

    ADDED 3-01_Rollercoaster_V1.py

---
+++ Day 03/Exercises/3-01_Rollercoaster_V1.py
@@ -0,0 +1,9 @@
+print("Welcome to the Rollercoaster!")
+
+height = int(input("What is your Height (in cm) ? "))
+
+# Conditional Statements
+if height > 120:
+    print("You are Eligible for Ride.")
+else:
+    print("You are Not Eligible.")
