1c299568190f8a326278ed0e80e9c7c64e4a8cb1
chaitanya_mokkapati <chaitanyamokkapati6@gmail.com>
2025-01-09 12:38:25 +0530

    Added comments

--- Day 02/Project/P2_TipCalculator.py
+++ Day 02/Project/P2_TipCalculator.py
@@ -4,9 +4,11 @@ totalBill = float(input("What was the Total Bill? $ "))
 peopleSplit = int(input("No of People to Split? "))
 tipPercentage = int(input("What percentage of tip would you like to give? \n10, 12, 15 \n"))
 
-# 
+# Calcuate Tip payable
 tipPercentageofTotalBill = totalBill * (tipPercentage / 100)
+# Add tip to Total Bill
 Actual_Bill = tipPercentageofTotalBill + totalBill
-payableForEach = Actual_Bill /peopleSplit
+# Split Total Bill accroding to user input
+payableForEach = Actual_Bill / peopleSplit
 
 print(f"Each person should pay: $ {payableForEach:.2f}")
