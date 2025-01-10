dd16b10dcd208e4c923d5ec6344d32862978e55f
chaitanya_mokkapati <chaitanyamokkapati6@gmail.com>
2025-01-10 14:29:36 +0530

    Added 5-04-02_AddOnlyEVenNumbers.py

---
+++ Day 05/Exercises/5-04-02_AddOnlyEVenNumbers.py
@@ -0,0 +1,13 @@
+'''
+You are going to write a program that calculates the sum of all the even numbers from 1 to 100,
+including 1 and 100
+'''
+
+total_sum = 0
+
+for number in range (1, 101):
+    
+    if number % 2 == 0:
+        total_sum += number
+    
+print("The sum of all even numbers from 1 to 100 is:", total_sum)
