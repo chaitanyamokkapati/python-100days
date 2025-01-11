08db40053106f2e9c9d895b181d9475ad507fe81
chaitanyamokkapati <chaitanyamokkapati6@gmail.com>
2025-01-11 06:09:42 +0530

    Added 6-01_Function.py

---
+++ Day 06/Exercises/6-01_Function.py
@@ -0,0 +1,13 @@
+# def is used to creating or defining function
+# def my_function():  -- syntax
+# function call to trigger it
+
+# Eg 1
+
+# creation of function
+def greeting():
+    print("--" * 10)
+    print("Good Morning!!!")
+    print("--" * 10)
+
+greeting() # function call
