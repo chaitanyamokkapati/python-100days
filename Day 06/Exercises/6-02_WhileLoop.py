e6aa040c6830e37936352045233536934f818f91
chaitanyamokkapati <chaitanyamokkapati6@gmail.com>
2025-01-11 15:26:57 +0530

    UPDATED 6-02_WhileLoop.py

--- Day 06/Exercises/6-02_WhileLoop.py
+++ Day 06/Exercises/6-02_WhileLoop.py
@@ -1,1 +1,23 @@
 # While loop - Do something Repeatedly
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+for n in range(6):
+    jump()
+
+no_hurd = 6
+while no_hurd > 0:
+    jump()
+    no_hurd -=1
